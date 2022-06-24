7cfead10a0d4c12deeabbb318264260d94a45deb
Gabriel Bigardi <gabrielbigardigames@gmail.com>
2022-06-24 12:56:07 -0300

    Removed custom editor as it was not needed, removed private variables with getters for less duplication, removed errors on built application by using preprocess instructions

--- Runtime/HierarchySeparator.cs
+++ Runtime/HierarchySeparator.cs
@@ -1,65 +1,22 @@
 using UnityEngine;
 using UnityEditor;
 
+#if UNITY_EDITOR
 [InitializeOnLoad]
+#endif
 public class HierarchySeparator : MonoBehaviour
 {
-    [HideInInspector]
-    [SerializeField]
-    private Color m_OutlineColor = Color.black;
-    public Color OutlineColor
-    {
-        get => m_OutlineColor;
-        set
-        {
-            value.a = 1f;
-            m_OutlineColor = value;
-        }
-    }
+    public Color OutlineColor;
+    public Color BarColor;
+    public Color TextColor;
+    public int OutlineSize;
 
-    [HideInInspector]
-    [SerializeField]
-    private Color m_BarColor = Color.black;
-    public Color BarColor
-    {
-        get => m_BarColor;
-        set
-        {
-            value.a = 1f;
-            m_BarColor = value;
-        }
-    }
-
-    [HideInInspector]
-    [SerializeField]
-    private Color m_TextColor = Color.white;
-    public Color TextColor
-    {
-        get => m_TextColor;
-        set
-        {
-            value.a = 1f;
-            m_TextColor = value;
-        }
-    }
-
-    [HideInInspector]
-    [SerializeField]
-    private int m_OutlineSize = 0;
-    public int OutlineSize
-    {
-        get => m_OutlineSize;
-        set
-        {
-            m_OutlineSize = value;
-        }
-    }
-
+#if UNITY_EDITOR
     static HierarchySeparator()
     {
         EditorApplication.hierarchyWindowItemOnGUI += HierarchyWindowItemOnGUI;
     }
-    
+
     [MenuItem("GameObject/Separator", false, 30)]
     public static void CreateSeparator(MenuCommand menuCommand)
     {
@@ -69,7 +26,7 @@ public class HierarchySeparator : MonoBehaviour
         Undo.RegisterCreatedObjectUndo(separator, "Create " + separator.name);
         Selection.activeObject = separator;
     }
-    
+
     static void HierarchyWindowItemOnGUI(int instanceID, Rect selectionRect)
     {
         GameObject gameObject = EditorUtility.InstanceIDToObject(instanceID) as GameObject;
@@ -86,19 +43,10 @@ public class HierarchySeparator : MonoBehaviour
         EditorGUI.DrawRect(new Rect(selectionRect.x + hierarchy.OutlineSize, selectionRect.y + hierarchy.OutlineSize, selectionRect.width - (hierarchy.OutlineSize * 2), selectionRect.height - (hierarchy.OutlineSize * 2)), hierarchy.BarColor);
         EditorGUI.DropShadowLabel(selectionRect, $"{gameObject.name.ToUpperInvariant()}", guiStyle);
     }
-
-    public void Start ()
-    {
-        gameObject.SetActive(false);
-    }
-    
-    public void Reset()
-    {
-        gameObject.tag = "EditorOnly";
-    }
 	
 	void OnValidate()
     {
         EditorApplication.RepaintHierarchyWindow();
     }
+#endif
 }
